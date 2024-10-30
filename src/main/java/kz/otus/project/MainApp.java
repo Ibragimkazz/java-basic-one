ca84c9c3e54af186f6b6e29f3dbf4f5ff3bae33f
Ibragim <kazzhanovibragim@gmail.com>
2024-10-30 18:19:12 +0600

    init commit

---
+++ src/main/java/kz/otus/project/MainApp.java
@@ -0,0 +1,16 @@
+package kz.otus.project;
+
+public class MainApp  {
+    public static void main(String[] args) {
+        greetings();
+    }
+
+    public static void greetings() {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("from");
+        System.out.println("Java");
+    }
+}
+
+
